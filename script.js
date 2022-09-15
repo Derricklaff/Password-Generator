73e8d33668ce921e1cdcde4c363fa7563ae31241
Derrick Lafforthun <derricklaff@gmail.com>
2022-09-15 15:46:47 -0700

    added funciton generatePassword nad prompts

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  length = prompt("How long should the password be?");
+  lowercase = confirm("Would you like lowercase letters in the password?");
+  uppercase = confirm("Would you like uppercase letters in the password?");
+  numeric = confirm("would you like numbers in the password?");
+  specialChar = confirm("Would you like to have special characters in the password?");
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +19,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+generatePassword()
